f6f1efe158a8e24fdba0874ab8dfa11978b26135
Nick Borsuk <gor_nick987@mail.ru>
2016-11-27 19:40:17 +0300

    ?поиск нетерменалов ?вставка на нужное место ?нужный илентификатор

--- BNI-2016/BNI-2016/GEN.cpp
+++ BNI-2016/BNI-2016/GEN.cpp
@@ -12,51 +12,53 @@
 #define STACK ".stack 4096\n"
 #define LINE_BREAK '\n'
 #define TITLE ".586\n.model flat, stdcall\n\nincludelib kernel32.lib\n"
-#define GEN1(tt) sprintf_s()
-#define GEN2(tt, val) std::string(tt)+std::string(val)
-#define T0_0 
 
-#define r "ret"
+#define GEN2(b, tmp, var1,var2)	sprintf_s(b, 1024, tmp, var1,var2)
+#define GEN1(b, tmp, var1)	sprintf_s(b, 1024, tmp, var1)
+#define GEN0(b, tmp)		sprintf_s(b,1024,tmp)
+#define T0_0 "%s PROC uses eax ebx ecx edi esi\n ;F\n ;N\n pop eax\n ret\n%s ENDP \n;S\n\n"
+#define T0_1 "%s PROC uses eax ebx ecx edi esi\n ;F\n ;N\n pop eax\nret\n%s ENDP \n\n"
+#define T0_2 "%s PROC uses eax ebx ecx edi esi\n ;F\n pop eax\n ret\n%s ENDP \n;S \n\n"
+#define T0_3 "%s PROC uses eax ebx ecx edi esi\n ;F\n pop eax\n ret\n%s ENDP\n\n"
+#define T0_4 "%s PROC uses eax ebx ecx edi esi\n ;N\n pop eax\n ret\n%s ENDP\n\n"
+#define T0_5 "%s PROC uses eax ebx ecx edi esi\n ;N\n pop eax\n ret\n%s ENDP\n;S\n\n"
+
 
 void Gen::StartGen(LEX::Lex lex, MFST::Mfst mfst, Log::LOG log, Parm::PARM parm)
 {
 	std::string gencode;
-	gencode += GEN2("fi", "koko");
+	std::cout << "________________________________\n";
 	gencode += TITLE;
 	gencode += LINE_BREAK;
 	gencode += CreateProtSeg(lex);
 	gencode += LINE_BREAK;
 	gencode += STACK;
-	gencode += SPACE;
+	gencode += LINE_BREAK;
+	gencode += LINE_BREAK;
+
+	gencode += MainGen(lex, mfst);
 	//gencode += CreateDatSeg(lex);
 	//gencode += CreateConstSeg(lex);
 
 
-	MFST::MfstState state;
-	GRB::Rule rule;
-	std::cout << "\nДерево разбора\n";
-	char rbuf[205];
 
-	for (unsigned short k = 0; k < mfst.storestate.size(); k++)
-	{
-		state = mfst.storestate._Get_container()[k];
-		rule = mfst.grebach.getRule(state.nrule);
-		std::cout << rule.getCRule(rbuf, state.nrulechain);
-	}
 	
 
 std::cout << gencode;
 *(log.stream) << "\n\n\nКод асм\n\n\n" << gencode;
 
 }
-std::string MainGen(LEX::Lex lex, MFST::Mfst mfst)
+std::string Gen::MainGen(LEX::Lex lex, MFST::Mfst mfst)
 {
 	MFST::MfstState state;
 	GRB::Rule rule;
 	std::string tmp;
-	for (unsigned short k = 0; k < mfst.storestate.size(); k++)
+	char buf[10240];
+	int k = 0;
+	int id = 0;
+	for (unsigned short i = 0; i < mfst.storestate.size(); i++)
 	{
-		state = mfst.storestate._Get_container()[k];
+		state = mfst.storestate._Get_container()[i];
 		rule = mfst.grebach.getRule(state.nrule);
 
 		switch (state.nrule)
@@ -66,8 +68,28 @@ std::string MainGen(LEX::Lex lex, MFST::Mfst mfst)
 			switch (state.nrulechain)
 			{
 			case 0:
-				//tmp += 
-				//тут вставляем шаблон для 0 правила 0 цепочки
+				k += GEN2(buf + k, T0_0, lex.idtable.table[id].id, lex.idtable.table[id].id);
+				id++;
+				break;
+			case 1:
+				k += GEN2(buf + k, T0_1, lex.idtable.table[id].id, lex.idtable.table[id].id);
+				id++;
+				break;
+			case 2:
+				k += GEN2(buf + k, T0_2, lex.idtable.table[id].id, lex.idtable.table[id].id);
+				id++;
+				break;
+			case 3:
+				k += GEN2(buf + k, T0_3, lex.idtable.table[id].id, lex.idtable.table[id].id);
+				id++;
+				break;
+			case 4:
+				k += GEN2(buf + k, T0_4, lex.idtable.table[id].id, lex.idtable.table[id].id);
+				id++;
+				break;
+			case 5:
+				k += GEN2(buf + k, T0_5, lex.idtable.table[id].id, lex.idtable.table[id].id);
+				id++;
 				break;
 			}
 			break;
@@ -76,72 +98,13 @@ std::string MainGen(LEX::Lex lex, MFST::Mfst mfst)
 		}
 
 
+		//std::cout << "-----------------------------\n" << buf;
 
 	}
-	return tmp;
+	return std::string(buf);
 }
 
-std::string Gen::CreateDatSeg(LEX::Lex lex)
-{
-	std::string tmp;
-	tmp += "\n.data\n";
-	for (int i = 0; i < lex.idtable.size; i++)
-	{
-		if (lex.idtable.table[i].idtype == IT::V)
-		{
-			tmp += lex.idtable.table[i].id;
-			tmp += DB;
-			switch (lex.idtable.table[i].iddatatype)
-			{
-			case IT::INT:
-				tmp += VAL_INT_DEFAULT;
-				break;
-			case IT::STR:
-				tmp += VAL_STR_DEFAULT;
-				break;
-			case IT::BOOL:
-				tmp += VAL_BOOL_DEFAULT;
-				break;
-			}
-			tmp += LINE_BREAK;
-		}
-	}
-	return tmp;
-}
-std::string Gen::CreateConstSeg(LEX::Lex lex)
-{
-	std::string tmp;
-	tmp += "\n.const\n";
-	for (int i = 0; i < lex.idtable.size; i++)
-	{
-		if (lex.idtable.table[i].idtype == IT::L)
-		{
-			tmp += lex.idtable.table[i].id;
-			tmp += DB;
-			switch (lex.idtable.table[i].iddatatype)
-			{
-			case IT::INT:
-				char bufint[10];
-				_itoa(lex.idtable.table[i].value.vind,bufint,10);
-				tmp += bufint;
-				break;
-			case IT::STR:
-				
-				tmp += lex.idtable.table[i].value.vstr.str;
-				tmp += ", 0";
-				break;
-			case IT::BOOL:
-				char boolint[10];
-				_itoa(lex.idtable.table[i].value.vind, boolint, 10);
-				tmp += boolint;
-				break;
-			}
-			tmp += LINE_BREAK;
-		}
-			
-	}
-	return tmp;
-}
+
 std::string Gen::CreateProtSeg(LEX::Lex lex)
 {
 	std::string tmp;
@@ -164,3 +127,65 @@ std::string Gen::CreateProtSeg(LEX::Lex lex)
 	return tmp;
 
 }
+
+//std::string Gen::CreateDatSeg(LEX::Lex lex)
+//{
+//	std::string tmp;
+//	tmp += "\n.data\n";
+//	for (int i = 0; i < lex.idtable.size; i++)
+//	{
+//		if (lex.idtable.table[i].idtype == IT::V)
+//		{
+//			tmp += lex.idtable.table[i].id;
+//			tmp += DB;
+//			switch (lex.idtable.table[i].iddatatype)
+//			{
+//			case IT::INT:
+//				tmp += VAL_INT_DEFAULT;
+//				break;
+//			case IT::STR:
+//				tmp += VAL_STR_DEFAULT;
+//				break;
+//			case IT::BOOL:
+//				tmp += VAL_BOOL_DEFAULT;
+//				break;
+//			}
+//			tmp += LINE_BREAK;
+//		}
+//	}
+//	return tmp;
+//}
+//std::string Gen::CreateConstSeg(LEX::Lex lex)
+//{
+//	std::string tmp;
+//	tmp += "\n.const\n";
+//	for (int i = 0; i < lex.idtable.size; i++)
+//	{
+//		if (lex.idtable.table[i].idtype == IT::L)
+//		{
+//			tmp += lex.idtable.table[i].id;
+//			tmp += DB;
+//			switch (lex.idtable.table[i].iddatatype)
+//			{
+//			case IT::INT:
+//				char bufint[10];
+//				_itoa(lex.idtable.table[i].value.vind, bufint, 10);
+//				tmp += bufint;
+//				break;
+//			case IT::STR:
+//
+//				tmp += lex.idtable.table[i].value.vstr.str;
+//				tmp += ", 0";
+//				break;
+//			case IT::BOOL:
+//				char boolint[10];
+//				_itoa(lex.idtable.table[i].value.vind, boolint, 10);
+//				tmp += boolint;
+//				break;
+//			}
+//			tmp += LINE_BREAK;
+//		}
+//
+//	}
+//	return tmp;
+//}
